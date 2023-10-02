4420868a7ef52e1b8a6bfa1c90fd743a8d7d42ad
虹輝 山本 <nanaki0203.y@gmail.com>
2023-10-02 11:03:08 +0900

    02_03

--- main.cpp
+++ main.cpp
@@ -3,9 +3,12 @@
 
 using namespace std;
 
-int32_t main()
+int32_t main(int32_t argc, char* argv[])
 {
-	cout << "Hello,World" << endl;
+	for (uint32_t i = 0; i < argc; i++)
+	{
+		cout << argv[i] << endl;
+	}
 
 	system("pause");
 	return 0;
